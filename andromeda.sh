a85c1e276a412a868993b039654b18a5d741c471
saasV1 <167147707+saasV1@users.noreply.github.com>
2024-04-28 20:49:46 -0300

    Update andromeda.sh

--- andromeda.sh
+++ andromeda.sh
@@ -6,12 +6,6 @@ if [ "$(id -u)" -ne 0 ]; then
     sudo su
 fi
 
-# Verifica se o usuário está no diretório /root/
-if [ "$PWD" != "/root" ]; then
-    echo "Mudando para o diretório /root/"
-    cd /root || exit
-fi
-
 # Verifica se está usando Ubuntu 20.04
 if ! grep -q 'Ubuntu 20.04' /etc/os-release; then
     echo "Este script recomenda o uso do Ubuntu 20.04."
@@ -24,14 +18,14 @@ sudo apt-get install -y dialog
 sudo apt update
 sudo apt upgrade -y
 
-# Verifica se o arquivo andromeda.v1 já existe
-if [ -e "SetupOrion" ]; then
+# Verifica se o arquivo andromeda.v1 já existe e remove, se existir
+if [ -e "andromeda.v1" ]; then
     echo "O arquivo andromeda.v1 já existe. Removendo..."
     rm andromeda.v1
 fi
 
-# Baixa o script
-https://bit.ly/SinergiaV1Lucrativa
+# Baixa o script e o salva como andromeda.v1
+curl -sSL https://raw.githubusercontent.com/saasV1/andromeda.v1/main/andromeda.sh -o andromeda.v1
 
 # Verifica se o download foi bem-sucedido
 if [ $? -eq 0 ]; then
@@ -39,7 +33,7 @@ if [ $? -eq 0 ]; then
     chmod +x andromeda.v1
     ./andromeda.v1
 else
-    echo "Falha ao baixar o script SetupOrion"
+    echo "Falha ao baixar o script andromeda.v1"
 fi
 
 sudo apt update
