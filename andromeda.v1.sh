2979aa62f47d753320e224b46a672cb06a1bd44e
saasV2 <newapexstor23@gmail.com>
2024-11-07 02:35:34 -0300

    Update andromeda.v1.sh

--- andromeda.v1.sh
+++ andromeda.v1.sh
@@ -5,7 +5,7 @@
 ## // ## // ## // ## // ## // ## // ## // ## //## // ## // ## // ## // ## // ## // ## // ## // ##
 
 versao() {
-echo -e "                                   \e[97mVersão do SetupOrion: \e[32mv. 2.5.9\e[0m                                  "
+echo -e "                                   \e[97mVersão do SetupAndromeda: \e[32mv. 2.5.9\e[0m                                  "
 echo -e "\e[32https://chat.whatsapp.com/Ez8z14o9SxV1w1Ya3clxtM     \e[97m<----- Grupos no WhatsApp ----->     \e[32https://chat.whatsapp.com/IpwTyjt9Gun4DQymh7bISo[0m"
 }
 
